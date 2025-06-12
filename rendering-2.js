924a0bce317a4eea1f80b54e04ef515bef63da23
sk-44 <88765725+sk-44@users.noreply.github.com>
2025-06-13 08:28:13 +0900

    switch文から連想配列に変更

--- rendering-2.js
+++ rendering-2.js
@@ -1,15 +1,13 @@
 function animalImgUrl(animal) {
     animal = animal.toLowerCase();
-    switch (animal) {
-        case 'dog':
-            return 'https://cdn.pixabay.com/photo/2017/09/25/13/12/dog-2785074__340.jpg';
-        case 'cat':
-            return 'https://cdn.pixabay.com/photo/2014/03/29/09/17/cat-300572__340.jpg';
-        case 'bird':
-            return 'https://cdn.pixabay.com/photo/2017/02/07/16/47/kingfisher-2046453__340.jpg';
-        default:
-            return 'https://cdn.pixabay.com/photo/2014/04/05/11/20/forest-315184__340.jpg';
-    }
+    
+    const animalUrl = {
+        dog: 'https://cdn.pixabay.com/photo/2017/09/25/13/12/dog-2785074__340.jpg',
+        cat: 'https://cdn.pixabay.com/photo/2014/03/29/09/17/cat-300572__340.jpg',
+        bird: 'https://cdn.pixabay.com/photo/2017/02/07/16/47/kingfisher-2046453__340.jpg',
+        other: 'https://cdn.pixabay.com/photo/2014/04/05/11/20/forest-315184__340.jpg'
+    };
+    return animalUrl[animal] || animalUrl['other'];
 }
 
 function renderAnimal(animal, elm) {
