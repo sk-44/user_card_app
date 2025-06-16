f2da75159948253600be2be5df4f9fdf10baaf19
sk-44 <88765725+sk-44@users.noreply.github.com>
2025-06-17 08:50:22 +0900

    関数に渡すものをクラスオブジェクトに変更

--- user-card.js
+++ user-card.js
@@ -28,7 +28,7 @@
 // 関数名：createEmployeeCard
 // 入力：employeeName, job, skills, country, avatarUrl
 // 出力：カードが含まれる要素(div)
-function createEmployeeCard(employeeName, job, skills, country, avatarUrl) {
+function createEmployeeCard(employee) {
     let innerFlex = document.createElement("div");
     innerFlex.classList.add("d-flex", "align-items-center", "col-md-7", "col-10", "m-1");
 
@@ -39,7 +39,7 @@ function createEmployeeCard(employeeName, job, skills, country, avatarUrl) {
     let leftHalf = document.createElement("div");
     leftHalf.classList.add("col-8", "py-3");
     let nameHeading = document.createElement("h4");
-    nameHeading.innerHTML = employeeName;
+    nameHeading.innerHTML = employee.getFullName();
     leftHalf.append(nameHeading);
 
     let paragraphDiv = document.createElement("div");
@@ -50,13 +50,13 @@ function createEmployeeCard(employeeName, job, skills, country, avatarUrl) {
     let countryParagraphDiv = paragraphDiv.cloneNode(true);
 
     let jobParagraphP = document.createElement("p");
-    jobParagraphP.innerHTML = "Job : <br>" + job;
+    jobParagraphP.innerHTML = "Job : <br>" + employee.job;
     jobParagraphDiv.append(jobParagraphP);
     let skillParagraphP = document.createElement("p");
-    skillParagraphP.innerHTML = "Skill : <br>" + skills;
+    skillParagraphP.innerHTML = "Skill : <br>" + employee.skills;
     skillParagraphDiv.append(skillParagraphP);
     let countryParagraphP = document.createElement("p");
-    countryParagraphP.innerHTML = "Country :<br>" + country;
+    countryParagraphP.innerHTML = "Country :<br>" + employee.country;
     countryParagraphDiv.append(countryParagraphP);
 
     leftHalf.append(jobParagraphDiv, skillParagraphDiv, countryParagraphDiv);
@@ -68,34 +68,56 @@ function createEmployeeCard(employeeName, job, skills, country, avatarUrl) {
     rightHalf.append(avatarDiv);
     let avatarImg = document.createElement("img");
     avatarImg.classList.add("avatar");
-    avatarImg.src = avatarUrl;
+    avatarImg.src = employee.avatarUrl;
     avatarDiv.append(avatarImg);
     cardDiv.append(rightHalf);
     
     return innerFlex;
 }
 
+class Employee {
+    constructor (firstName, lastName, job, skills, country, avatarUrl) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.job = job;
+        this.skills = skills;
+        this.country = country;
+        this.avatarUrl = avatarUrl;
+    }
+
+    getFullName() {
+        return this.firstName + " " + this.lastName;
+    }
+}
+
 const profile_card = document.getElementById("profiles")
 
-const employee1 = createEmployeeCard(
-    "Kaiden Herman",
+let employee1 = new Employee(
+    "Kaiden",
+    "Herman",
     "Software Engineer",
     "C++, C#, Java, PHP, JavaScript, Python",
     "United States",
     "https://pbs.twimg.com/profile_images/501759258665299968/3799Ffxy.jpeg"
 );
-const employee2 = createEmployeeCard(
-    "Elizabeth Dunn",
+let employee2 = new Employee(
+    "Elizabeth",
+    "Dunn",
     "Accountant",
     "Excel, Word, Quickbooks",
     "England",
     "https://randomuser.me/api/portraits/women/76.jpg"
 );
-const employee3 = createEmployeeCard(
-    "Duan Moreno",
+let employee3 = new Employee(
+    "Duan",
+    "Moreno",
     "Teacher",
     "Working with children, History, Word",
     "Argentina",
     "https://randomuser.me/api/portraits/med/men/93.jpg"
 );
+
+employee1 = createEmployeeCard(employee1);
+employee2 = createEmployeeCard(employee2);
+employee3 = createEmployeeCard(employee3);
 profile_card.append(employee1, employee2, employee3);
