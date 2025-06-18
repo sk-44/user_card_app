0a564eea9fc10f359ffae81774736f3b417abab8
sk-44 <88765725+sk-44@users.noreply.github.com>
2025-06-18 23:48:35 +0900

    motivation card 土台作成

---
+++ motivational-card.js
@@ -0,0 +1,26 @@
+
+
+
+let domObj = document.getElementById("target");
+
+motivationalSpeechWallpaper(
+    "Perfection is achieved, not when there is nothing more to add, but when there is nothing left to take away. - Antoine de Saint",
+    "1B4F72",
+    "https://cdn.pixabay.com/photo/2020/06/12/03/06/magnifying-glass-5288877__340.jpg",
+    "top",
+    "right",
+    domObj
+);
+motivationalSpeechWallpaper(
+    "The scientist discovers a new type of material or energy and the engineer discovers a new use for it. - Gordon Lindsay Glegg",
+    "007bff","https://cdn.pixabay.com/photo/2018/02/23/04/38/laptop-3174729_1280.jpg",
+    "center",
+    "left",
+    domObj
+);
+motivationalSpeechWallpaper("Scientists study the world as it is, engineers create the world that never has been. - Theodore von Karman",
+    "ecf0f1","https://cdn.pixabay.com/photo/2017/05/10/19/29/robot-2301646_1280.jpg",
+    "center",
+    "center",
+    domObj
+);
